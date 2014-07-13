7668798c2de8c1b0aa6a4d9d2e2fe18ee197711b
Christian Gärtner <christiangaertner.film@googlemail.com>
2014-07-13 15:34:38 +0200

    Fixed local run script

--- local.sh
+++ local.sh
@@ -6,6 +6,12 @@ export HUBOT_IRC_DEBUG=true
 export HUBOT_LOG_LEVEL="debug"
 export PORT=4000
 export HUBOT_MAIN_CHANNEL="#OompaLoompa"
+
+export HTTP_DAPING_NOTIFY="/notify"
+export HTTP_ROUTE_SHUTDOWN="/shutdown"
+export HTTP_UPTIME_DAPING="/daping"
+export HTTP_UPTIME_ROBOT="/robot"
+
 echo "RUN REDIS SERVER!!!!!!!!"
 
 ./bin/hubot -a irc -n "$HUBOT_IRC_NICK" -l oompa
